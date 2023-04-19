4fc4b4f15a180e8f8d57aeb6a4fc02c7bac0daa3
giomendes.quezada <gioq89@gmail.com>
2023-04-18 20:18:53 -0700

    added confirms for the types of characters

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,24 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
+
+  // add prompt to ask length of password to be inputed "I am presented with a series of prompts for password criteria and length of the password"
   var passwordLength = prompt("How many characters would you like your password to contain? (must be between 8 and 128)");
 
+  // add confirms to ask which tyoes of charactrs should be included "I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters"
+  var lowercase = confirm("abcdefghijklmnopqrstuvwxyz");
+  var uppercase = confirm("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  var numberic = confirm("0123456789");
+  var specialCharacterss = confirm("*.!@#$%^&(){}[]:;<>,.?/~_+-=|");
+
   if (passwordLength < 8 || passwordLength > 128) {
-    alert("Invalid password lentgh. Must be between 8 and 128 characters.")
+    alert("Invalid password lentgh. Must be between 8 and 128 characters.");
   }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
