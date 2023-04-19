812797a8e495514d8b81dfdfd148dbc187bcffb5
giomendes.quezada <gioq89@gmail.com>
2023-04-18 20:35:38 -0700

    added return so nothing is asked if wrong length

--- Develop/script.js
+++ Develop/script.js
@@ -10,12 +10,13 @@ function writePassword() {
 }
 
 function generatePassword() {
-
   // add prompt to ask length of password to be inputed "I am presented with a series of prompts for password criteria and length of the password"
   var passwordLength = prompt("How many characters would you like your password to contain? (must be between 8 and 128)");
+  
   // alert in case user selects wrong password length
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Invalid password length. Must be between 8 and 128 characters.");
+    return;
   }
 
   // add confirms to ask which types of characters should be included "I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters"
@@ -23,10 +24,7 @@ function generatePassword() {
   var uppercase = confirm("Do you want uppercase letters included?");
   var numberic = confirm("Do you want numbers included?");
   var specialCharacters = confirm("Do you want special characters included?");
-
-  
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
