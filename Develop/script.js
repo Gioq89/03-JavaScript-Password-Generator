f7d41d79a8bb3e2a1d3690fe6ce9c91c2e1e5842
giomendes.quezada <gioq89@gmail.com>
2023-04-18 20:54:40 -0700

    added alert in case user doesn't select anything

--- Develop/script.js
+++ Develop/script.js
@@ -12,7 +12,7 @@ function writePassword() {
 function generatePassword() {
   // add prompt to ask length of password to be inputed "I am presented with a series of prompts for password criteria and length of the password"
   var passwordLength = prompt("How many characters would you like your password to contain? (must be between 8 and 128)");
-  
+
   // alert in case user selects wrong password length
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Invalid password length. Must be between 8 and 128 characters.");
@@ -24,6 +24,12 @@ function generatePassword() {
   var uppercase = confirm("Do you want uppercase letters included?");
   var numberic = confirm("Do you want numbers included?");
   var specialCharacters = confirm("Do you want special characters included?");
+
+  // alert in case user doesn't select any character types "my input should be validated and at least one character type should be selected"
+  if(!lowercase && !uppercase && !numberic && !specialCharacters) {
+    alert("Invalid password. Must include at least one character type.");
+    return;
+  }
 }
 
 // Add event listener to generate button
